b2cf255f2908e281738c28750fb508113b3b761c
YenNhi <yennhi231102@gmail.com>
2024-07-21 19:44:43 +0700

    Template DAX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TrangChu from './page/users/TrangChu';
+import Shop from './page/users/Shop';
+import Footer from './component/Footer';
+import HeadID from './component/HeadID';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <HeadID/>
+        
+        <main>
+          <Routes>
+            <Route path="/" element={<TrangChu />} />
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+        </main>
+
+        <footer className="bg-dark" id="tempaltemo_footer">
+          <Footer/>
+        </footer>
+      </div>
+    </Router>
+
   );
 }
 
